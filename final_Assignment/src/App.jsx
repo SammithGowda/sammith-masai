52ee283ba00e0c69373debedd34f77c180716a05
Sammith1 <hsammith@gmail.com>
2022-04-17 20:02:00 +0530

    commit

--- final_Assignment/src/App.jsx
+++ final_Assignment/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
         <Route
           path="/add_teacher"
           element={
-            <PrivateRoute>
+            <PrivateRoute isauth={isauth}>
               {" "}
               <Add_teacher />
             </PrivateRoute>
